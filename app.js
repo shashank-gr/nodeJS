0fdee3c1acd0b34643c2a69742eb9e438bf2847b
shashank4214@gmail.com <shashank-gr>
2022-11-05 10:41:06 +0530

    hadeling req/res

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const http = require("http"); // importing the http module from node
 
 const server = http.createServer((req, res) => {
-  console.log("shashanka");
-  console.log(req);
-  res.write("<h1>Shashanka</h1>");
+  console.log(req.headers); //gives me the incoming request header
+  console.log(req.url); //gives me the incoming req URL
+  console.log(req.method); //gives the incoming req method; default: GET
+  const url = req.url; //getting the request url and storing it
+  res.setHeader("Content-Type", "text/html"); //setting the response header
+  // res.setHeader("Authentication", "auth tocken here");
+  if (url == "/home") {
+    res.write("<h1>Welcome Home</h1>");
+  } else if (url == "/about") {
+    res.write("<h1>Welcome to About Us Page</h1>");
+  } else if (url == "/node") {
+    res.write("<h1>Welcome to my Node Js project</h1>");
+  } else {
+    res.write("<h1>Enter valid route</h1>");
+  }
+
+  res.end();
 });
 
 server.listen(4000);
